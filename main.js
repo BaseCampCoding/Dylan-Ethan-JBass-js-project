1ee0b93223ab3ff7b1663dc2b946c7a1a095d381
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-04-26 15:12:35 -0500

    element block and load- css - javascript changes

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
+const body = document.getElementById("body");
 const mainScreen = document.getElementById("main-screen");
-const easyBtn = document.getElementById("easy")
-const medBtn = document.getElementById("medium")
-const hardBtn = document.getElementById("hard")
+const easyBtn = document.getElementById("easy");
+const medBtn = document.getElementById("medium");
+const hardBtn = document.getElementById("hard");
+
+function onLoad(){
+    mainScreen.style.display = "block";
+};
 
 easyBtn.addEventListener("click", function(){
-    mainScreen.classList.add("hideMain")
+    mainScreen.style.display = "none";
+    body.style.overflow = "scroll";
     easyMode()
 });
 
 medBtn.addEventListener("click", function(){
-    mainScreen.classList.add("hideMain")
+    mainScreen.style.display = "none";
+    body.style.overflow = "scroll";
     mediumMode()
 });
 
 hardBtn.addEventListener("click", function(){
-    mainScreen.classList.add("hideMain")
+    mainScreen.style.display = "none";
+    body.style.overflow = "scroll";
     hardMode()
 });
 
@@ -55,15 +63,18 @@ const game = {
 
 // EASY MODE
 function easyMode(){
-    console.log("Easy Mode")
+    console.log("Easy Mode");
+    
 };
 
 // MEDIUM MODE
 function mediumMode(){
-    console.log("Medium Mode")
+    console.log("Medium Mode");
+    
 };
 
 // HARD MODE
 function hardMode(){
-    console.log("Hard Mode")
+    console.log("Hard Mode");
+    
 };
